003a93bf1fa91dff76ebbb32f840b527942529c2
Matthew Carroll <matthew.j.carroll@outlook.com>
2019-04-29 10:38:08 -0500

    working version of client and server needs to be adapted for assignment

--- server.py
+++ server.py
@@ -1,16 +1,68 @@
+import sys
 import socket
+import selectors
+import types
+
 
 host = '127.0.0.1'
 port = 12619
+maxclients = 3
+user_info = {}
 
-with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-    s.bind((host, port))
-    s.listen()
-    conn, addr = s.accept()
-    with conn:
-        print("Connected by", addr)
-        while True:
-            data = conn.recv(1024)
-            if not data:
-                break
-            conn.sendall(data)
+sel = selectors.DefaultSelector()
+
+def accept_wrapper(sock):
+    conn, addr = sock.accept()
+    print('accepted connection from', addr)
+    conn.setblocking(False)
+    data = types.SimpleNamespace(addr=addr, inb=b'', outb=b'')
+    events = selectors.EVENT_READ | selectors.EVENT_WRITE
+    sel.register(conn, events, data=data)
+
+def service_connection(key, mask):
+    sock = key.fileobj
+    data = key.data
+    if mask & selectors.EVENT_READ:
+        recv_data = sock.recv(1024)
+        if recv_data:
+            data.outb += recv_data
+        else:
+            print('closing connection to', data.addr)
+            sel.unregister(sock)
+            sock.close()
+    if mask & selectors.EVENT_WRITE:
+        if data.outb:
+            print('echoing', repr(data.outb), 'to', data.addr)
+            sent = sock.send(data.outb)
+            data.outb = data.outb[sent:]
+
+
+lsock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+lsock.bind((host, port))
+lsock.listen()
+print('listening on', (host, port))
+lsock.setblocking(False)
+sel.register(lsock, selectors.EVENT_READ, data=None)
+
+try:
+    while True:
+        events = sel.select(timeout=None)
+        for key, mask in events:
+            if key.data is None:
+                accept_wrapper(key.fileobj)
+            else:
+                service_connection(key, mask)
+except KeyboardInterrupt:
+    print("Interrupted by user, exiting")
+finally:
+    sel.close()
+
+#general main function
+if __name__ == '__main__':
+    #open and read in file to dictionary containing user data
+    #this is terrible security practice, but I don't care at the moment
+    f = open('users.txt', 'r')
+    for line in f:
+        info = line.split(',')
+        user_info[info[0]] = info[1]
+    f.close()
